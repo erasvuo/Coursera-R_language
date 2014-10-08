ed45aeb590e7fbfd82c651d9b1dee522af6f2ed1
erasvuo <kari.erasvuo@gmail.com>
2014-10-08 08:58:14 +0300

    just testing again

--- myfunction.R
+++ myfunction.R
@@ -1,1 +1,14 @@
-dadaa
+myfunction <- function() {
+	x <- rnorm(100)
+	mean(x)
+}
+
+second <- function(x) {
+	x + rnorm(length(x))
+# comment added
+}
+
+third <- function(y) {
+	y + rnorm(length(y))
+# comment added
+}
